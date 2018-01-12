4b075b12a21671b781acdf53acd7206c586ebd8b
Alexander Gorban <gorban.alexander.y@gmail.com>
2018-01-12 20:08:57 +0200

    Working with Attribute-based Routing. Add VinylRecordsController

---
+++ AirVinyl.API/Controllers/VinylRecordsController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.OData;
+using System.Web.OData.Routing;
+using AirVinyl.DataAccessLayer;
+
+namespace AirVinyl.API.Controllers
+{
+    public class VinylRecordsController : ODataController
+    {
+        private AirVinylDbContext _ctx = new AirVinylDbContext();
+
+        [HttpGet]
+        [ODataRoute("VinylRecords")]
+        public IHttpActionResult GetAllVinylRecords()
+        {
+            return Ok(_ctx.VinylRecords);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _ctx.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
