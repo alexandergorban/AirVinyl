c7feb17392d2a97e5b8ee9eb54c10087c09ad3e7
Alexander Gorban <gorban.alexander.y@gmail.com>
2018-01-12 18:59:01 +0200

    Selecting an EntitySet. Added PeopleController

---
+++ AirVinyl.API/Controllers/PeopleController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.OData;
+using AirVinyl.DataAccessLayer;
+
+namespace AirVinyl.API.Controllers
+{
+    public class PeopleController : ODataController
+    {
+        private AirVinylDbContext _ctx = new AirVinylDbContext();
+
+        public IHttpActionResult Get()
+        {
+            return Ok(_ctx.People);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _ctx.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
