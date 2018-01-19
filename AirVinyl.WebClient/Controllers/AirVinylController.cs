9617f11b82aefada5a318f95c6a21bb9f3ab8308
Alexander Gorban <gorban.alexander.y@gmail.com>
2018-01-19 14:02:36 +0200

    Added AirVinyl ViewModel, Controller, View

---
+++ AirVinyl.WebClient/Controllers/AirVinylController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AirVinyl.Model;
+using AirVinyl.WebClient.Models;
+
+namespace AirVinyl.WebClient.Controllers
+{
+    public class AirVinylController : Controller
+    {
+        // GET: AirVinyl
+        public ActionResult Index()
+        {
+            var context = new AirVinylContainer(new Uri("http://localhost:15707/odata"));
+
+            var peopleResponse = context.People.Execute();
+            var personResponse = context.People.ByKey(1).GetValue();
+
+            return View(new AirVinylViewModel()
+            {
+                People = peopleResponse,
+                Person = personResponse
+            });
+        }
+    }
+}
---
+++ AirVinyl.WebClient/Models/AirVinylViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using AirVinyl.Model;
+
+namespace AirVinyl.WebClient.Models
+{
+    public class AirVinylViewModel
+    {
+        public IEnumerable<Person> People { get; set; }
+        public Person Person { get; set; }
+        public string AdditionalData { get; set; }
+
+        public AirVinylViewModel()
+        {
+            People = new List<Person>();
+        }
+    }
+}
