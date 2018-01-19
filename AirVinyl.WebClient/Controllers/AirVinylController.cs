42171edc524849e8b65e4e1795ea42bdde9f3a0f
Alexander Gorban <gorban.alexander.y@gmail.com>
2018-01-19 14:56:23 +0200

    Updated AirVinylController. POST new Person()

--- AirVinyl.WebClient/Controllers/AirVinylController.cs
+++ AirVinyl.WebClient/Controllers/AirVinylController.cs
@@ -16,27 +16,61 @@ namespace AirVinyl.WebClient.Controllers
         {
             var context = new AirVinylContainer(new Uri("http://localhost:15707/odata"));
 
-            var peopleResponse = context.People
-                .IncludeTotalCount()
-                .Expand(p => p.VinylRecords)
-                .Execute() as QueryOperationResponse<Person>;
+            //var peopleResponse = context.People
+            //    .IncludeTotalCount()
+            //    .Expand(p => p.VinylRecords)
+            //    .Execute() as QueryOperationResponse<Person>;
 
+            //var peopleAsList = peopleResponse.ToList();
+
+            //DataServiceQueryContinuation<Person> token = peopleResponse.GetContinuation();
+
+            //peopleResponse = context.Execute(token);
+            //peopleAsList = peopleResponse.ToList();
+
+            //string additionalData = "Total count:" + peopleResponse.TotalCount.ToString();
+
+
+            //// select people by FirstName.EnsWith("n")
+            //var peopleResponse = context.People
+            //    .Expand(p => p.VinylRecords)
+            //    .Where(p => p.FirstName.EndsWith("n"))
+            //    .OrderByDescending(p => p.FirstName)
+            //    .Skip(1)
+            //    .Take(1);
+
+            //var peopleAsList = peopleResponse.ToList();
+
+            //// selest names
+            //var selectFromPeople = context.People.Select(p => new {p.FirstName, p.LastName});
+
+            //string additionalData = "";
+            //foreach (var partialPerson in selectFromPeople)
+            //{
+            //    additionalData += partialPerson.FirstName + " " + partialPerson.LastName + "\n";
+            //}
+            //// 
+
+            //var personResponse = context.People.ByKey(1).GetValue();
+
+            // add new Person()
+            var newPerson = new Person()
+            {
+                FirstName = "Maggie",
+                LastName = "Smith"
+            };
+
+            context.AddToPeople(newPerson);
+            context.SaveChanges();
+
+            var peopleResponse = context.People.OrderByDescending(p => p.PersonId);
             var peopleAsList = peopleResponse.ToList();
-
-            DataServiceQueryContinuation<Person> token = peopleResponse.GetContinuation();
-
-            peopleResponse = context.Execute(token);
-            peopleAsList = peopleResponse.ToList();
-
-            string additionalData = "Total count:" + peopleResponse.TotalCount.ToString();
-
-            var personResponse = context.People.ByKey(1).GetValue();
 
             return View(new AirVinylViewModel()
             {
                 People = peopleAsList,
-                Person = personResponse,
-                AdditionalData = additionalData
+                //Person = personResponse,
+                //AdditionalData = additionalData
             });
         }
     }
